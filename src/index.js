883f0744553551a0c111c29c062fcfa76a9f1b5e
Fanbayy <fandi.bayu110@gmail.com>
2024-03-15 16:22:56 +0700

    added mobile layout version

--- src/index.js
+++ src/index.js
@@ -6,5 +6,51 @@ $(document).ready(function () {
     dots: true, // Tampilkan titik navigasi
     slidesToShow: 3,
     slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1026,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+});
+
+// scrolling top
+$(document).ready(function () {
+  // Show or hide the button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $("#scrollToTopBtn").fadeIn();
+    } else {
+      $("#scrollToTopBtn").fadeOut();
+    }
+  });
+
+  // Smooth scrolling to top
+  $("#scrollToTopBtn").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 800);
+    return false;
   });
 });
