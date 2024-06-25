a0b8758c82325a4cfab16eef19b287a30da66321
fanbayy <fandi.bayu110@gmail.com>
2024-06-25 16:23:11 +0700

    add dashboard pages

--- src/index.js
+++ src/index.js
@@ -89,10 +89,108 @@ document.addEventListener("DOMContentLoaded", function () {
       const errorMessage = document.getElementById("errorMessage");
 
       if (email === "kkp@mail.com" && password === "test123") {
-        window.location.href = "dashboard.html";
+        window.location.href = "artikelDashboard.html";
       } else {
         errorMessage.textContent = "Email atau password anda salah!";
       }
+    });
+  }
+});
+
+const dropZone = document.getElementById("dropZone");
+
+dropZone.addEventListener("click", function () {
+  document.getElementById("fileInput").click();
+});
+
+document
+  .getElementById("fileInput")
+  .addEventListener("change", handleFileSelect);
+
+function handleDragOver(event) {
+  event.preventDefault();
+  dropZone.classList.add("border-primary");
+}
+
+function handleDragLeave(event) {
+  event.preventDefault();
+  dropZone.classList.remove("border-primary");
+}
+
+function handleDrop(event) {
+  event.preventDefault();
+  dropZone.classList.remove("border-primary");
+
+  const files = event.dataTransfer.files;
+  handleFiles(files);
+}
+
+function handleFileSelect(event) {
+  const files = event.target.files;
+  handleFiles(files);
+}
+
+function handleFiles(files) {
+  const fileSizeLimit = 10 * 1024 * 1024; // 10 MB in bytes
+
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    if (file.size > fileSizeLimit) {
+      alert("File size exceeds the limit of 10 MB.");
+      return;
+    }
+    // You can handle the file here (e.g., upload it to server)
+    alert.log("File selected:", file.name);
+  }
+}
+
+// uplod file div
+
+const bpsToggle = document.getElementById("bps");
+const uncomToggle = document.getElementById("uncom");
+const bpsDiv = document.getElementById("bpsDiv");
+const uncomDiv = document.getElementById("uncomDiv");
+
+uncomToggle.addEventListener("click", () => {
+  if (bpsToggle.classList.contains("active")) {
+    bpsToggle.classList.remove("active");
+    uncomToggle.classList.add("active");
+    bpsDiv.classList.add("d-none");
+    uncomDiv.classList.remove("d-none");
+    bpsToggle.addEventListener("click", () => {
+      bpsToggle.classList.add("active");
+      uncomToggle.classList.remove("active");
+      bpsDiv.classList.remove("d-none");
+      uncomDiv.classList.add("d-none");
+    });
+  }
+});
+
+const uplodButton = document.getElementById("uplodBtn");
+const uplodTable = document.getElementById("uplodTable");
+
+uplodButton.addEventListener("click", () => {
+  if (uplodTable.classList.contains("d-none")) {
+    uplodTable.classList.toggle("d-none");
+  }
+});
+
+const adminCmsToggle = document.getElementById("adminCms");
+const adminApiToggle = document.getElementById("adminApi");
+const cmsDiv = document.getElementById("cmsDiv");
+const apiDiv = document.getElementById("apiDiv");
+
+adminApiToggle.addEventListener("click", () => {
+  if (adminCmsToggle.classList.contains("active")) {
+    adminCmsToggle.classList.remove("active");
+    adminApiToggle.classList.add("active");
+    cmsDiv.classList.add("d-none");
+    apiDiv.classList.remove("d-none");
+    adminCmsToggle.addEventListener("click", () => {
+      adminCmsToggle.classList.add("active");
+      adminApiToggle.classList.remove("active");
+      cmsDiv.classList.remove("d-none");
+      apiDiv.classList.add("d-none");
     });
   }
 });
